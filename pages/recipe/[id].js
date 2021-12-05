85813e4a0ca3bdbbb41764c91676e0911415f854
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2021-12-05 00:20:27 -0400

    Added recipe informations

--- pages/recipe/[id].js
+++ pages/recipe/[id].js
@@ -1,50 +1,86 @@
 import React from "react";
-import { List, ListItem, Typography, TextField } from "@mui/material";
+import { List, ListItem, Typography, Container, Grid } from "@mui/material";
 import { BASE_URL } from "../../utils/config";
 import axios from "axios";
+import { recipe } from "../../utils/mock";
+import Image from "next/image";
 
 const Recipe = ({ data }) => {
   console.log(data);
   return (
-    <List>
-      <ListItem>
-        <Typography>Recipe Page</Typography>
-        <Typography></Typography>
-      </ListItem>
-      <ListItem>
-        <TextField
-          variant="outlined"
-          fullWidth
-          id="search"
-          label="Search for recipes"
-          inputProps={{ type: "text" }}
-        />
-      </ListItem>
-    </List>
+    <Container>
+      <Typography component="h1" variant="h2" align="center">
+        {data?.title}
+      </Typography>
+      <Grid container justifyContent="center">
+        <Grid item xs={12} md={5}>
+          <Image
+            src={data.image}
+            alt={data.title}
+            width={720}
+            height={500}
+            layout="responsive"
+          ></Image>
+        </Grid>
+      </Grid>
+      <List>
+        <ListItem>
+          <Typography component="h4" variant="h4">
+            Ingredients
+          </Typography>
+        </ListItem>
+        {data?.extendedIngredients.map((ing) => {
+          return <ListItem key={ing.id}>{ing.original}</ListItem>;
+        })}
+      </List>
+      <List>
+        <ListItem>
+          <Typography component="h4" variant="h4">
+            Instructions
+          </Typography>
+        </ListItem>
+        {data?.analyzedInstructions.map((section) => {
+          return section.steps.map((step, i) => {
+            return (
+              <Typography key={i}>
+                {i + 1} - {step.step}
+              </Typography>
+            );
+          });
+        })}
+      </List>
+    </Container>
   );
 };
 
 export default Recipe;
 
 export async function getServerSideProps(context) {
-  const { params } = context;
-  const { id } = params;
+  const mock = true;
+  let result;
+  if (!mock) {
+    const { params } = context;
+    const { id } = params;
 
-  const url = `${BASE_URL}/${id}/information`;
-  const options = {
-    params: {
-      apiKey: process.env.API_KEY,
-    },
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+    const url = `${BASE_URL}/${id}/information`;
+    const options = {
+      params: {
+        apiKey: process.env.API_KEY,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-  const { data } = await axios.get(url, options);
+    const { data } = await axios.get(url, options);
+    result = data;
+  } else {
+    result = recipe;
+  }
 
   return {
     props: {
-      data,
+      data: result,
     },
   };
 }
